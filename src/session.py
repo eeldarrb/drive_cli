8a41162204fe962de2f8047f05e88e49416aa303
eeldarrb <167944058+eeldarrb@users.noreply.github.com>
2025-06-07 17:43:01 -0400

    update display messages when using unknown command and exiting repl loop

--- src/session.py
+++ src/session.py
@@ -10,6 +10,7 @@ def cli_loop():
             cmd, *args = user_line.split()
             if cmd in command_map:
                 command_map[cmd](*args)
-
+            else:
+                print(f"drive_cli: command not found: {cmd}")
     except KeyboardInterrupt:
-        print("Exiting...")
+        print("\nExiting...")
