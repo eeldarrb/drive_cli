642a417cd31e885dd543cb5aa9d0707c1706dfad
eeldarrb <167944058+eeldarrb@users.noreply.github.com>
2025-06-21 18:13:58 -0400

    save token after completing auth flow
    - allows user's session to persist (don't have to signin on google oauth
    server every time)
    - implementation from google drive api python quickstart

--- src/auth/google_auth.py
+++ src/auth/google_auth.py
@@ -1,6 +1,10 @@
+import os.path
+from google.oauth2.credentials import Credentials
+from google.auth.transport.requests import Request
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 
+TOKEN_FILE = "token.json"
 CLIENT_SECRETS_FILE = "credentials.json"
 SCOPES = ["https://www.googleapis.com/auth/drive"]
 API_SERVICE_NAME = "drive"
@@ -8,7 +12,19 @@ API_VERSION = "v3"
 
 
 def get_auth_service():
-    flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRETS_FILE, SCOPES)
-    creds = flow.run_local_server(host="localhost", port=0)
+    creds = None
+
+    if os.path.exists(TOKEN_FILE):
+        creds = Credentials.from_authorized_user_file(TOKEN_FILE, SCOPES)
+
+    if creds and creds.expired and creds.refresh_token:
+        creds.refresh(Request())
+
+    if not creds or not creds.valid:
+        flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRETS_FILE, SCOPES)
+        creds = flow.run_local_server(host="localhost", port=0)
+        with open(TOKEN_FILE, "w") as token:
+            token.write(creds.to_json())
+
     service = build(API_SERVICE_NAME, API_VERSION, credentials=creds)
     return service
