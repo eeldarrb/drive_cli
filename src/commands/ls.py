f741aa6b3cd0594246ab76924064dd6582a55e17
eeldarrb <167944058+eeldarrb@users.noreply.github.com>
2025-07-08 21:18:40 -0400

    modify ls command to use tree structure

--- src/commands/ls.py
+++ src/commands/ls.py
@@ -1,6 +1,8 @@
 def handle_ls(session, *args):
-    client = session.client
-
-    items = client.list_folder_items()
-    for item in items:
-        print(f"{item['name']}")
+    # client = session.client
+    #
+    # items = client.list_folder_items()
+    # for item in items:
+    #     print(f"{item['name']}")
+    for item in session.cwd.children:
+        print(item.name)
