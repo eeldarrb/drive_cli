20ceb4ce65a971c93e3157408d9fd8a652b61c52
eeldarrb <167944058+eeldarrb@users.noreply.github.com>
2025-07-26 13:32:46 -0400

    feat: add fuzzy match suggestion

--- src/session.py
+++ src/session.py
@@ -5,9 +5,12 @@ from drive_tree.drive_tree import build_drive_tree
 from services.drive_client import DriveClient
 from google.auth.exceptions import GoogleAuthError
 from googleapiclient.http import HttpError
+from rapidfuzz import process
 
 
 class Session:
+    INPUT_FUZZY_MATCH_THRESHOLD = 80
+
     def __init__(self):
         try:
             self.client = DriveClient(get_auth_service)
@@ -27,13 +30,15 @@ class Session:
 
                 cmd, *args = shlex.split(user_line)
                 cmd_info = command_map.get(cmd)
-                # TODO: add fuzzy match suggestion message for commands?
+
                 if not cmd_info:
                     print(f"Command not found: '{cmd}'")
+                    best_match, score, _ = process.extractOne(cmd, command_map.keys())
+                    if score > self.INPUT_FUZZY_MATCH_THRESHOLD:
+                        print(f"Did you mean '{best_match}'?")
                     continue
 
                 expected_args = cmd_info.get("args", [])
-
                 if len(args) != len(expected_args):
                     usage = cmd_info.get("usage")
                     print(f"Usage: {cmd} {usage}")
