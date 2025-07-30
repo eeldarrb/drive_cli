f70bb163a9da41c72ebb89cab9b9c7df1d7fe3b9
eeldarrb <167944058+eeldarrb@users.noreply.github.com>
2025-07-29 20:37:01 -0400

    handle HTTPError in session instead of in util, modify error message

--- src/services/drive_client_utils.py
+++ src/services/drive_client_utils.py
@@ -4,20 +4,21 @@ from googleapiclient.http import HttpError
 
 
 def retry_on_http_error(max_attempts=2, delay=1):
-	def decorator(func):
-		@functools.wraps(func)
-		def wrapper(*args, **kwargs):
-			attempt = 0
-			err_status = None
-			while attempt < max_attempts:
-				try:
-					return func(*args, **kwargs)
-				except HttpError as e:
-					err_status = e.status_code
-					time.sleep(delay)
-					attempt += 1
-			raise RuntimeError(f"HTTP Error. Server responded with {err_status}.")
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            attempt = 0
+            http_err = None
+            while attempt < max_attempts:
+                try:
+                    return func(*args, **kwargs)
+                except HttpError as e:
+                    time.sleep(delay)
+                    attempt += 1
+                    http_err = e
+            if http_err is not None:
+                raise http_err
 
-		return wrapper
+        return wrapper
 
-	return decorator
+    return decorator
