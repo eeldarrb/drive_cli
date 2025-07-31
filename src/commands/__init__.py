a83c94b695d645a13f53da02241fab524991e160
eeldarrb <167944058+eeldarrb@users.noreply.github.com>
2025-07-30 22:04:59 -0400

    chore(command_map): cleanup unused args and usage

--- src/commands/__init__.py
+++ src/commands/__init__.py
@@ -7,19 +7,11 @@ from .mkdir import handle_mkdir
 from .cd import handle_cd
 
 command_map = {
-    "cd": {"handler": handle_cd, "args": ["folder_name"], "usage": "<folder_name>"},
-    "ls": {"handler": handle_ls, "args": [], "usage": ""},
-    "clear": {"handler": handle_clear, "args": [], "usage": ""},
-    "mkdir": {
-        "handler": handle_mkdir,
-        "args": ["folder_name"],
-        "usage": "<folder_name>",
-    },
-    "rm": {"handler": handle_rm, "args": ["item_name"], "usage": "<item_name>"},
-    "download": {
-        "handler": handle_download,
-        "args": ["item_name"],
-        "usage": "<item_name>",
-    },
-    "upload": {"handler": handle_upload, "args": ["file_path"], "usage": "<file_path>"},
+    "cd": {"handler": handle_cd},
+    "ls": {"handler": handle_ls},
+    "clear": {"handler": handle_clear},
+    "mkdir": {"handler": handle_mkdir},
+    "rm": {"handler": handle_rm},
+    "download": {"handler": handle_download},
+    "upload": {"handler": handle_upload},
 }
