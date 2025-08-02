34c75198e3908537ce942ad9dc95c2d31447c769
eeldarrb <167944058+eeldarrb@users.noreply.github.com>
2025-08-02 14:26:54 -0400

    refactor: convert drive_tree to class
    - change from 3 tightly coupled functions to 1 method for building tree

--- src/drive_tree/drive_tree.py
+++ src/drive_tree/drive_tree.py
@@ -1,31 +1,33 @@
 from .drive_node import DriveNode
 
 
-def build_drive_tree(client):
-    drive_files = client.list_all_files()
-    drive_nodes = build_node_map(drive_files)
+class DriveTree:
+    def __init__(self, client):
+        self.client = client
+        self.root = self._build()
 
-    root_id = client.get_root_id()
-    root_node = DriveNode(root_id, "/", mime_type="application/vnd.google-apps.folder")
-    drive_nodes[root_id] = root_node
+    def _build(self):
+        all_files = self.client.list_all_files()
+        root_id = self.client.get_root_id()
 
-    link_nodes(drive_files, drive_nodes)
-    return root_node
+        # Create dictionairy of DriveNodes using files
+        drive_nodes = {
+            file["id"]: DriveNode(
+                file.get("id"), file.get("name"), file.get("mimeType")
+            )
+            for file in all_files
+        }
+        drive_nodes[root_id] = DriveNode(
+            root_id, "/", mime_type="application/vnd.google-apps.folder"
+        )
 
+        # Link DriveNodes together into tree
+        for file in all_files:
+            node = drive_nodes.get(file.get("id"))
+            parent_ids = file.get("parents", [])
+            for parent_id in parent_ids:
+                parent_node = drive_nodes.get(parent_id)
+                if parent_node:
+                    parent_node.add_child(node)
 
-def build_node_map(files):
-    return {
-        file["id"]: DriveNode(file.get("id"), file.get("name"), file.get("mimeType"))
-        for file in files
-    }
-
-
-def link_nodes(files, node_map):
-    for file in files:
-        node = node_map.get(file.get("id"))
-        parent_ids = file.get("parents", [])
-
-        for parent_id in parent_ids:
-            parent_node = node_map.get(parent_id)
-            if parent_node:
-                parent_node.add_child(node)
+        return drive_nodes[root_id]
