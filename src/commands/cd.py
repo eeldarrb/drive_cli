ffa51b41ac130d89ea93e2f0fefb1d7472797382
eeldarrb <167944058+eeldarrb@users.noreply.github.com>
2025-08-04 00:54:44 -0400

    modify cd to allow for relative paths

--- src/commands/cd.py
+++ src/commands/cd.py
@@ -6,12 +6,8 @@ def handle_cd(session, *args):
     parser.add_argument("directory_name")
     parsed = parser.parse_args(args)
 
-    directory_name = parsed.directory_name
-    for file in session.cwd.children:
-        if (
-            file.name == directory_name
-            and file.mime_type == "application/vnd.google-apps.folder"
-        ):
-            session.cwd = file
-            return
-    raise FileNotFoundError(directory_name)
+    directory_path = parsed.directory_name
+    session.cwd = session.drive_tree.get_node_by_path(
+        session.cwd, directory_path, require_directory=True
+    )
+    return
