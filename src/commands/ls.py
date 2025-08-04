c07f2fc150219eabde6e811e547bf2148a25fd80
eeldarrb <167944058+eeldarrb@users.noreply.github.com>
2025-08-04 01:01:23 -0400

    feat: implement ability to use ls on a provided path

--- src/commands/ls.py
+++ src/commands/ls.py
@@ -3,7 +3,11 @@ import argparse
 
 def handle_ls(session, *args):
     parser = argparse.ArgumentParser(prog="ls")
+    parser.add_argument("path", nargs="?", default=".")
     parsed = parser.parse_args(args)
 
-    for file in session.cwd.children:
+    path = parsed.path
+    resolved_path = session.drive_tree.get_node_by_path(session.cwd, path)
+
+    for file in resolved_path.children:
         print(file.name)
