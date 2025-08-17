07ada363d0d002277954ea51bda6592399e6f9a7
eeldarrb <167944058+eeldarrb@users.noreply.github.com>
2025-08-17 17:03:13 -0400

    feat(ls): print file name if path resolves to a file
    - mimic unix behaviour of printing out the file, previously nothing
    would happen

--- src/commands/ls.py
+++ src/commands/ls.py
@@ -8,5 +8,8 @@ def handle_ls(session, *args):
 
     resolved_node = session.vfs.ls(parsed.path)
 
-    for file in resolved_node.children:
-        print(file.name)
+    if resolved_node.is_folder():
+        for file in resolved_node.children:
+            print(file.name)
+    else:
+        print(resolved_node.name)
