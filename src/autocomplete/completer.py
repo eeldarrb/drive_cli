ca2acb92f75d72459cf55df146d374a07e323562
eeldarrb <167944058+eeldarrb@users.noreply.github.com>
2025-08-17 16:38:59 -0400

    feat(completer): only show directories as completion options for the "cd" command

--- src/autocomplete/completer.py
+++ src/autocomplete/completer.py
@@ -27,11 +27,15 @@ def create_completer(vfs, command_map):
                 basename = os.path.basename(path)
 
                 try:
+                    require_dir = cmd == "cd"
+
                     resolved_node = vfs._resolve_node_by_path(dirname)
                     candidate_nodes = resolved_node.children
 
                     for candidate_node in candidate_nodes:
-                        if candidate_node.name.startswith(basename):
+                        if require_dir and not candidate_node.is_folder():
+                            continue
+                        elif candidate_node.name.startswith(basename):
                             if os.path.dirname(path) == "":
                                 completion = candidate_node.name
                             else:
