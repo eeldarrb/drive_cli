f83a9736bd40be5b4042ecb875953d2f734ffc16
eeldarrb <167944058+eeldarrb@users.noreply.github.com>
2025-08-18 22:57:24 -0400

    refactor(node): node as parent template class, drive_node sets folder_mimetype
    - node logic now located in Node instead of DriveNode
    - DriveNode converted to subclass of Node
    - is_folder() checks using folder_mimetype (set in drive_node)

---
+++ src/vfs/node.py
@@ -0,0 +1,34 @@
+class Node:
+    folder_mimetype = None
+
+    def __init__(self, id, name, mime_type):
+        self.id = id
+        self.name = name
+        self.mime_type = mime_type
+        self.parent = None
+        self.children = []
+
+    def is_folder(self):
+        return self.mime_type == self.folder_mimetype
+
+    def add_child(self, child_node):
+        self.children.append(child_node)
+        child_node.parent = self
+
+    def remove_child(self, child_node):
+        if child_node in self.children:
+            self.children.remove(child_node)
+            child_node.parent = None
+
+    def detach(self):
+        if self.parent:
+            self.parent.remove_child(self)
+            self.parent = None
+
+    def get_path(self):
+        if self.parent is None:
+            return "/"
+        if self.parent.get_path() == "/":
+            return f"/{self.name}"
+
+        return f"{self.parent.get_path()}/{self.name}"
